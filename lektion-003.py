24f7d639034e3f369f3ece84cb1dd887d7a77d9a
Eken6591 <emil91ek@gmail.com>
2026-09-04 14:56:02 +0200

    Add files via upload

---
+++ lektion-003.py
@@ -0,0 +1,21 @@
+x = 42
+y = 13.37
+text = "Hello, world!"
+isit = True
+noitisnt = False
+none = none
+
+ids = [1, 2, 3]
+task = {"id": 1, "task": "chilla hela helgen"}
+
+todos = [
+    {"id": ids: [0], "task": "chilla hela helgen"}
+    {"id": ids:[1], "task": "åka tåg"}
+]
+
+print(todos[0]["task"])
+
+
+print("saker jag behöver göra:\n")
+for todo in todos:
+    print(f"{todo["id"]}. {todo["task"]}")
